d02c000293f2bb95a24bbf11db33450f37887b2a
James <James4224@hotmail.co.uk>
2022-01-22 16:25:50 +0000

    Simple conjugate gradient method implementation

--- 1.2 Intermediate Julia/Problem2.jl
+++ 1.2 Intermediate Julia/Problem2.jl
@@ -1,3 +1,23 @@
 # Operators
+# define type StrangMatix
 
-define type StrangMatix
+
+function StrangMatrixMultiply!(y,x)
+# function to apply the strang matrix to a vector in an operator stype form
+    y[1] = 2*x[1] + x[2]
+    for i in 2:(length(x)-1)
+        y[i] = x[i-1] + 2*x[i] + x[i+1]
+    end
+    i = length(x)
+    y[i] = x[i-1] + 2*x[i]
+end
+
+function MyMain()
+    x = ones(4,1)
+    y = zeros(4,1)
+
+    StrangMatrixMultiply!(y,x)
+    print(y)
+end
+
+MyMain()
